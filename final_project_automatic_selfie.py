6720ae6f863f33eb87d6906ef0400540754a399d
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-02-09 05:37:57 +0800

    Adding some code that will change the color

--- final_project_automatic_selfie.py
+++ final_project_automatic_selfie.py
@@ -22,3 +22,6 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
