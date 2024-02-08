86e889727d84c0d3dc13394dab036b650212dd9e
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-02-09 05:30:20 +0800

    Install necessary modules

--- final_project_automatic_selfie.py
+++ final_project_automatic_selfie.py
@@ -4,4 +4,10 @@
 
 # Creating pseudocode
 
-
+# Import necessary modules
+import cv2
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
